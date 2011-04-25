3715282d738823512af345d0ff8367612e54c5f7
Kunal <rails@kunalinfotech.net>
2011-04-25 18:05:04 +0530

    changes

--- app/models/emailer.rb
+++ app/models/emailer.rb
@@ -1,7 +1,7 @@
 class Emailer < ActionMailer::Base
     def contact_email(contact)
       setup_email(contact)
-      @subject    += '#{contact.subject}'
+      @subject    += 'conatct-us'
      
     end
 
